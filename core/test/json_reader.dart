3ee89e0a21a64ec967bf6a320d903c5db0e41813
Muhammad Shofwan Qobus <shofwanqobus@upi.edu>
2022-06-22 11:51:47 +0700

    edit json_reader

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -2,8 +2,14 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
+
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+
+  if (dir.endsWith('core')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+
+  return File('$dir/core/test/$name').readAsStringSync();
 }
